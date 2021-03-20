be4ccc5cf499ab30c30444d80010f22aa78acae1
Levi-Dominguez <josue.dominguez2@g.austincc.edu>
2021-03-20 15:52:59 -0700

    Completed 03

--- problems/03-shopping-list.js
+++ problems/03-shopping-list.js
@@ -18,12 +18,25 @@ addToClothesList('pants'); // returns ['tshirt', 'pants']
 console.log(clothesList); // ['tshirt', 'pants']
 
 ***********************************************************************/
+/* Understand:
+    1. write a function "shoppingList" that accepts a arr as an arg(list).
+    2. the function will then return a new function that will add to the initial function arg(list)
+    3. when the new funciton is invoked it will return the initial list with the new items.
+   Plan:
+    1. create a function with the arg as list.
+    2. inside the function return a function with the arg (items).
+    3. inside the new function push the arg (items) into the shoppingList arg (list)
+    4. return the list.
+*/
 
-function shoppingList(list) {
-
-  // your code here
-
-}
+function shoppingList(list)
+{
+  return function(items)
+  {
+    list.push(items)
+    return list;
+  }
+};
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = shoppingList;
