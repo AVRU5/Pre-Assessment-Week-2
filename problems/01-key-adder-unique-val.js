3c8d13f40d239c39a0919babacae2982a08af092
Levi-Dominguez <josue.dominguez2@g.austincc.edu>
2021-03-20 15:20:28 -0700

    Added notes to 01

--- problems/01-key-adder-unique-val.js
+++ problems/01-key-adder-unique-val.js
@@ -13,6 +13,18 @@ keyAdderUniqueVal(cat, "fruit", "orange"); // => {name: "Willie", color: "orange
 console.log(cat); // { name: "Willie", color: "orange", toy: "yarn" }
 ***********************************************************************/
 
+
+/* Understand:
+	1. write a function that accepts an object, key and value as parameters. the key and value should pair together in the object.
+	2. when function is invoked the funciton should immediately assign the key and value arguement as a key pair, but only if not already in object.
+	3. once completed, return the mutated object.
+   Plan:
+    1. create the function keyAdderUniqueVal with the parameters (object, key, value).
+	2. assign a variable that will pull the values of an object, essentially creating an array of all the values.
+	3. create an if statement that say if the argument value is not included into the values array (from step 2),
+	   then assign the key argument into the object with the value arguement as the new value for that key.
+
+*/
 function keyAdderUniqueVal(object, key, value)
 {
 	let values = Object.values(object); //Object.keys()
