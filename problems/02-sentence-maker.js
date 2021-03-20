21bc4788be01a0a0abd133233e89b8403d1d7479
Levi-Dominguez <josue.dominguez2@g.austincc.edu>
2021-03-20 15:39:18 -0700

    Completed 02

--- problems/02-sentence-maker.js
+++ problems/02-sentence-maker.js
@@ -12,7 +12,23 @@ sentenceMaker('The', 'quick', 'brown', 'fox', 'jumps', 'over', 'the', 'lazy', 'd
 ***********************************************************************/
 
 // your code here
+/* Understand:
+	1. write a function using fat arrow syntax, it will accept a infinite amount of arguments.
+	2. create a sentence out of the arguments, add an "!" to the end of the sentence.
+   Plan:
+    1. write a function using fat arrow syntax, use the reduce parameter syntax.
+	2. create a for loop to iterate through the new arr
+	3. return the arguments as string with an "!" at then end.
+*/
 
+let sentenceMaker = (...arg) =>
+{
+
+	for(let i = 0; i < arg.length; i++)
+	{
+		return arg.join(" ") + "!"
+	}
+};
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
 	module.exports = sentenceMaker;
