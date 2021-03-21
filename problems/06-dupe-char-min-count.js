0a0490dfff95189301e228fd41258418f0e939f3
Levi-Dominguez <josue.dominguez2@g.austincc.edu>
2021-03-20 17:51:18 -0700

    Completed 06

--- problems/06-dupe-char-min-count.js
+++ problems/06-dupe-char-min-count.js
@@ -10,11 +10,41 @@ duplicateCharMinCount("apple", 2) // ["p"]
 duplicateCharMinCount("banana", 2) // ["a", "n"]
 duplicateCharMinCount("What about a longer string?", 3) // ["a", "t", " "]
 ***********************************************************************/
+/* Understand:
+	1. Write a function that takes in a string and num as arguments. (string, minCount)
+	2. Function will then return an array of characters that show up ass many times as minCount.
+	3. The String will have at least 1 character.
+   Plan:
+    1.
+*/
+function duplicateCharMinCount(string, minCount)
+{
 
-function duplicateCharMinCount(string, minCount) {
+	let stringCounter = {};
+	let res = [];
 
-	// your code here
+	// count up how many times each character shows up from the string parameter
+	for(let i = 0; i < string.length; i++)
+	{
+		let current_letter = string[i]
+		if(stringCounter[current_letter])
+		{
+			stringCounter[current_letter] += 1;
+		}
+		else
+		{
+			stringCounter[current_letter] = 1;
+		}
+	}
 
+	for(letter in stringCounter)
+	{
+		if(stringCounter[letter] >= minCount)
+		{
+			res.push(letter)
+		}
+	}
+	return res;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
