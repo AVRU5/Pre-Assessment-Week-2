32012078286008e88d1c9d1ae6d9ddb1fb8e57ef
Levi-Dominguez <josue.dominguez2@g.austincc.edu>
2021-03-20 17:13:41 -0700

    Completed 05

--- problems/05-filter-user-profiles.js
+++ problems/05-filter-user-profiles.js
@@ -19,8 +19,28 @@ const facebookUsers = [
 filterUserProfiles(facebookUsers, (user) => user.age > 30); // ["Mary", "Judy", "Mike"]
 filterUserProfiles(facebookUsers, (user) => user.state === "New York"); // ["Mike"]
 ***********************************************************************/
-
+/* Understand:
+    1. write a function called "filterUserProfiles" that accepts an arr and a cb as arguments.
+    2. the cb function will take in the arr's obj and return true or false for the obj.
+    3. the function should use the cb to return an arr of names when passed into the cb function, return true.
+    4. all user profiles will pass in with the key of "name".
+   Plan:
+    1. write the function "filterUserProfiles" with the arg (arr, cb).
+*/
 // your code here
+let filterUserProfiles = (arr, cb) =>
+{
+  let newArr = [];
+  for(let i = 0; i < arr.length; i++)
+  {
+    let el = arr[i]
+    if (cb(el) === true)
+    {
+      newArr.push(el.name)
+    }
+  }
+  return newArr;
+};
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
